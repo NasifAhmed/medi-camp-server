39f3b43411f609680453aaac07ba8ee79a7e448d
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-24 22:56:40 +0600

    Add fixes and morgan

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const globalErrorHandler = require("./utils/globalErrorHandler");
 const connectDB = require("./utils/connectDB");
 const applyMiddleware = require("./middlewares/applyMiddlewares");
@@ -6,13 +7,13 @@ const applyMiddleware = require("./middlewares/applyMiddlewares");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/health", (req, res) => {
+app.get("/health", morgan(`tiny`), (req, res) => {
     res.send("Server is running............");
 });
 
 applyMiddleware(app);
 
-app.all("*", (req, res, next) => {
+app.all("*", morgan(`tiny`), (req, res, next) => {
     const error = new Error(`Can't find ${req.originalUrl} on the server`);
     error.status = 404;
     next(error);
