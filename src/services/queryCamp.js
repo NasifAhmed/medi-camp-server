f324b0558834ab4edd3f89f2304858a6d72fadc9
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-25 13:04:05 +0600

    Add query function for camps

---
+++ src/services/queryCamp.js
@@ -0,0 +1,18 @@
+const camp = require("../models/camp");
+
+async function queryCamp(filter) {
+    const sortField = {};
+
+    if (filter.sort) {
+        if (filter.sort.startsWith("-")) {
+            sortField[filter.sort.slice(1)] = -1;
+        } else {
+            sortField[filter.sort] = 1;
+        }
+        delete filter.sort;
+    }
+    const cursor = await camp.find(filter).sort(sortField);
+    return cursor;
+}
+
+module.exports = queryCamp;
